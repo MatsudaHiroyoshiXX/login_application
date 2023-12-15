9bd8a410909aadbe4d1031bde5bd4d347669f304
MatsudaHiroyoshiF <hiroyoshi.pro@gmail.com>
2023-12-15 09:34:50 +0900

    test commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,9 +18,7 @@ const Title = styled.h1`
 const Title2 = styled.h1`
   font-size: 24px;
 `;
-const Title3 = styled.h1`
-  font-size: 24px;
-`;
+
 
 const UserImage = styled.img`
   height: 40px; // 画像の高さを調整
