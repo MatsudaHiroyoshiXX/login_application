c82995839db0907c5fcf8e8e5f3db2172a7b7e88
zarakiima <zakivideo.98@gmail.com>
2024-02-08 12:00:31 +0900

    CustomerList-Sort-Fullversion

--- src/pages/CustomerListMap.tsx
+++ src/pages/CustomerListMap.tsx
@@ -10,51 +10,66 @@ import DownArrow from '../img/DownArrow.png';
 const CustomerListMap = () => {
   
     const [searchTerm, setSearchTerm] = useState('');
+    const [sortOrder, setSortOrder] = useState('asc');
+    const [hoveredColumn, setHoveredColumn] = useState<null | string>(null);
+
+    interface SortIconProps {
+      isHovered: boolean;
+      handleSort: () => void;
+    }
+
+    interface Customer {
+      name: string; // 名前
+      carNumber: string; // 車両ナンバー
+      inspectionExpiryDate: string; // 車検満了日
+      currentCar: string; // 現在の車
+      proposedCar: string; // 提案された車
+      videoUrl: string; // 動画URL
+      updateDate: string; // 更新日
+      [key: string]: string;
+    }
     
-    const handleSearch = () => {
-      
-    };
-    const [customers, setCustomers] = useState([
+    const [customers, setCustomers] = useState<Customer[]>([
       { 
-        name: 'や山田 太郎', 
+        name: 'あ山田 太郎', 
         carNumber: '東京 200 は 2019',
-        inspectionExpiryDate: '2023/04/01',
+        inspectionExpiryDate: '2023/07/11',
         currentCar: 'プリウスUグレード',
         proposedCar: 'アルファードXグレード',
         videoUrl: 'http://example.com/video/a',
-        updateDate: '2023/01/01'
+        updateDate: '2023/03/24'
       },
       { 
-        name: 'す鈴木 花子', 
+        name: 'か鈴木 花子', 
         carNumber: '神奈川 300 さ 2020',
         inspectionExpiryDate: '2023/05/01',
         currentCar: 'フィットLグレード',
         proposedCar: 'シビックSグレード',
         videoUrl: 'http://example.com/video/b',
-        updateDate: '2023/02/01'
+        updateDate: '2023/10/22'
       },
       { 
         name: 'さ佐藤 二郎', 
         carNumber: 'さいたま 400 は 2021',
-        inspectionExpiryDate: '2023/06/01',
+        inspectionExpiryDate: '2023/01/01',
         currentCar: 'エスティマPグレード',
         proposedCar: 'ハイエースHグレード',
         videoUrl: 'http://example.com/video/a',
-        updateDate: '2023/03/01'
+        updateDate: '2023/08/30'
       },
       { 
-        name: 'き北野 悠人', 
+        name: 'た北野 悠人', 
         carNumber: '旭川 500 は 2022',
-        inspectionExpiryDate: '2023/07/01',
+        inspectionExpiryDate: '2023/06/10',
         currentCar: 'ヴェゼルRグレード',
         proposedCar: 'オデッセイYグレード',
         videoUrl: 'http://example.com/video/a',
-        updateDate: '2023/04/01'
+        updateDate: '2023/11/23'
       },
       { 
-        name: 'よ寄谷 海斗', 
+        name: 'な寄谷 海斗', 
         carNumber: '札幌 600 は 2023',
-        inspectionExpiryDate: '2023/08/01',
+        inspectionExpiryDate: '2023/08/17',
         currentCar: 'ハリアーDグレード',
         proposedCar: 'ヤリスGグレード',
         videoUrl: 'http://example.com/video/a',
@@ -62,24 +77,15 @@ const CustomerListMap = () => {
       },
     ]);
 
-    const [isHovered, setIsHovered] = useState(false);
-    
-    const handleMouseEnter = () => {
-      setIsHovered(true);
-    };
-  
-    const handleMouseLeave = () => {
-      setIsHovered(false);
+    const handleSearch = () => {
     };
     
-    const [sortOrder, setSortOrder] = useState('asc');
-
-    const handleSort = () => {
+    const handleSort = (sortBy: keyof Customer) => {
       const sortedCustomers = [...customers].sort((a,b)=> {
         if (sortOrder === 'asc') {
-          return a.name.localeCompare(b.name,'ja-JP');
+          return a[sortBy].localeCompare(b[sortBy],'ja-JP');
         } else {
-          return b.name.localeCompare(a.name,'ja-JP');
+          return b[sortBy].localeCompare(a[sortBy],'ja-JP');
         }
       });
 
@@ -87,12 +93,14 @@ const CustomerListMap = () => {
       setSortOrder((prevOrder) => (prevOrder === 'asc' ? 'desc' : 'asc'));
     };
 
-    const ArrowImage = () => {
-      return <div onClick={handleSort} style={{position: 'absolute', right:0,top: '50%', transform: 'translateY(-50%)'}}>
+    const ArrowImage: React.FC<SortIconProps> = ({isHovered, handleSort}) => (
+      isHovered ? (
+      <div onClick={handleSort} style={{position: 'absolute', right:0,top: '50%', transform: 'translateY(-50%)'}}>
         <img src={UpArrow} alt="up" style={{ cursor: 'pointer', display: 'block', width: 'auto', height: '7.54px' }}/>
         <img src={DownArrow} alt="down" style={{ cursor: 'pointer', display: 'block', width: 'auto', height: '7.54px', marginTop:5 }}/>
       </div>
-    }
+        ) : null
+      )
 
   return (
     <Root>
@@ -119,15 +127,41 @@ const CustomerListMap = () => {
         <thead>
           <TableRow>
             <TableHeader 
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-              >{isHovered && (<ArrowImage/>)}
-              お名前</TableHeader>
-            <TableHeader>車両ナンバー</TableHeader>
-            <TableHeader>車検満了日</TableHeader>
-            <TableHeader>乗り換え提案</TableHeader>
+              onMouseEnter={() => setHoveredColumn('name')}
+              onMouseLeave={() => setHoveredColumn(null)}
+              >
+                <ArrowImage isHovered={hoveredColumn === 'name'} handleSort={() => handleSort('name')} />
+              お名前
+            </TableHeader>
+            <TableHeader
+              onMouseEnter={() => setHoveredColumn('carNumber')}
+              onMouseLeave={() => setHoveredColumn(null)}
+              >
+                <ArrowImage isHovered={hoveredColumn === 'carNumber'} handleSort={() => handleSort('carNumber')} />
+              車両ナンバー
+            </TableHeader>
+            <TableHeader
+              onMouseEnter={() => setHoveredColumn('inspectionExpiryDate')}
+              onMouseLeave={() => setHoveredColumn(null)}
+              >
+                <ArrowImage isHovered={hoveredColumn === 'inspectionExpiryDate'} handleSort={() => handleSort('inspectionExpiryDate')} />
+              車検満了日
+            </TableHeader>
+            <TableHeader
+              onMouseEnter={() => setHoveredColumn('currentCar')}
+              onMouseLeave={() => setHoveredColumn(null)}
+              >
+                <ArrowImage isHovered={hoveredColumn === 'currentCar'} handleSort={() => handleSort('currentCar')} />
+              乗り換え提案
+            </TableHeader>
             <TableHeader>動画URL</TableHeader>
-            <TableHeader>更新日</TableHeader>
+            <TableHeader
+              onMouseEnter={() => setHoveredColumn('updateDate')}
+              onMouseLeave={() => setHoveredColumn(null)}
+              >
+                <ArrowImage isHovered={hoveredColumn === 'updateDate'} handleSort={() => handleSort('updateDate')} />
+              更新日
+            </TableHeader>
             <TableHeader>詳細&動画編集</TableHeader>
           </TableRow>
         </thead>
@@ -163,38 +197,46 @@ const Root = styled.div`
 const PageTitle = styled.h1`
 `
 const SearchBox = styled.div`
+  width: 752px;
   height: 57px;
+  margin: auto;
   position: relative; 
   display: flex;
-  justify-content: center;
-  align-items: centers
+  justify-content: space-between;
+  align-items: centers;
+  border-bottom: solid rgba(224, 224, 224, 1);
 `
 
 const SearchTextField = styled.input`
-  width: 752px;
+  width: 100%;
   border: none;
   outline: none;
-  border-bottom: solid rgba(224, 224, 224, 1);
 `
 const SearchButton = styled.button`
-  position: absolute;
-  transform: translateX(500%);
-  margin-top: -22px;
+  position: relative;
   height: 44px;
   width: 68px;
-  top:50%;
+  top:5px;
   display: flex;
   align-items: center;
   justify-content: center;
-  cursor:pointer;
+  cursor: pointer;
   background-color: #0000cd;
   border-radius: 99px;
   border: none;
+  box-shadow:0 4px 0 #dcdcdc;
+  &:hover{
+    opacity: 0.8;
+  }
+  &:active{
+    box-shadow: none;
+    top: 9px;
+  }
 `
 const StartIcon = styled.div`
   color: white;
-  position: absolute;
-  top:10px;
+  position: relative;
+  top:2px;
 `
 const Table = styled.table`
   margin-top: 45px;
