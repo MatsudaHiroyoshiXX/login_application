792a9ff168a6bb396dc207dad5f6cfbdb8e78e72
rikiyoshie <riki335geji@gmail.com>
2024-02-14 10:45:32 +0900

    CustomersData

---
+++ src/data/CustomersData.ts
@@ -0,0 +1,47 @@
+export const CustomersData = [
+    { 
+    name: 'あ山田 太郎', 
+    carNumber: '東京 200 は 2019',
+    inspectionExpiryDate: '2023/07/11',
+    currentCar: 'プリウスUグレード',
+    proposedCar: 'アルファードXグレード',
+    videoUrl: 'http://example.com/video/a',
+    updateDate: '2023/03/24'
+    },
+    { 
+    name: 'か鈴木 花子', 
+    carNumber: '神奈川 300 さ 2020',
+    inspectionExpiryDate: '2023/05/01',
+    currentCar: 'フィットLグレード',
+    proposedCar: 'シビックSグレード',
+    videoUrl: 'http://example.com/video/b',
+    updateDate: '2023/10/22'
+    },
+    { 
+    name: 'さ佐藤 二郎', 
+    carNumber: 'さいたま 400 は 2021',
+    inspectionExpiryDate: '2023/01/01',
+    currentCar: 'エスティマPグレード',
+    proposedCar: 'ハイエースHグレード',
+    videoUrl: 'http://example.com/video/a',
+    updateDate: '2023/08/30'
+    },
+    { 
+    name: 'た北野 悠人', 
+    carNumber: '旭川 500 は 2022',
+    inspectionExpiryDate: '2023/06/10',
+    currentCar: 'ヴェゼルRグレード',
+    proposedCar: 'オデッセイYグレード',
+    videoUrl: 'http://example.com/video/a',
+    updateDate: '2023/11/23'
+    },
+    { 
+    name: 'な寄谷 海斗', 
+    carNumber: '札幌 600 は 2023',
+    inspectionExpiryDate: '2023/08/17',
+    currentCar: 'ハリアーDグレード',
+    proposedCar: 'ヤリスGグレード',
+    videoUrl: 'http://example.com/video/a',
+    updateDate: '2023/05/01'
+    },
+]
